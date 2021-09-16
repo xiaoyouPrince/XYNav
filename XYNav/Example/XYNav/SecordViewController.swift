9d4504478a208f782846eab478b339782ac5ed6b
xiaoyou.qu <xiaoyouPrince@163.com>
2021-09-16 22:58:13 +0800

    新增加一个VC测试用

--- XYNav/Example/XYNav/SecordViewController.swift
+++ XYNav/Example/XYNav/SecordViewController.swift
@@ -1,32 +1,27 @@
 //
-//  DestViewController.swift
-//  XYNav
+//  SecordViewController.swift
+//  XYNav_Example
 //
-//  Created by xy on 2021/9/14.
+//  Created by 渠晓友 on 2021/9/16.
+//  Copyright © 2021 CocoaPods. All rights reserved.
 //
 
 import UIKit
 
-class DestViewController: UIViewController {
+class SecordViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .green
+        self.view.backgroundColor = .systemPink
         
-        self.title = "你好"
+        self.title = "第二个页面"
         if #available(iOS 14.0, *) {
             self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: "返回", style: .plain, target: self, action: #selector(back))
         } else {
             // Fallback on earlier versions
         }
-        
-        let btn = UIButton()
-        self.view.addSubview(btn)
-        btn.setTitle("去新页面", for: .normal)
-        btn.frame = CGRect(x: 100, y: 200, width: 200, height: 40)
-        btn.addTarget(self, action: #selector(gotoNewPage), for: .touchUpInside)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,9 +33,7 @@ class DestViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
-    @objc
-    func gotoNewPage() {
-        let detail = SecordViewController()
-        self.navigationController?.pushViewController(detail, animated: true)
-    }
+
+    
+
 }
