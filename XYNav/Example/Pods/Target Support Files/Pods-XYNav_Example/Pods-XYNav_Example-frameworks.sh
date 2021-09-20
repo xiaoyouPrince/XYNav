3a75d3973cf4c6ef497737d482717f50f54263b4
xiaoyou.qu <xiaoyouPrince@163.com>
2021-09-20 18:43:08 +0800

    增加 demo

--- XYNav/Example/Pods/Target Support Files/Pods-XYNav_Example/Pods-XYNav_Example-frameworks.sh
+++ XYNav/Example/Pods/Target Support Files/Pods-XYNav_Example/Pods-XYNav_Example-frameworks.sh
@@ -175,9 +175,13 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/XYInfomationSection/XYInfomationSection.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/XYNav/XYNav.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/XYInfomationSection/XYInfomationSection.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/XYNav/XYNav.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
