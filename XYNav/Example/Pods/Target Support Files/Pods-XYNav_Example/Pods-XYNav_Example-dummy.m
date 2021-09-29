fb907d6df832005302e0880936d5a3013cee5b7a
xiaoyou.qu <xiaoyouPrince@163.com>
2021-09-29 23:18:26 +0800

    update .gitignore

--- XYNav/Example/Pods/Target Support Files/Pods-XYNav_Example/Pods-XYNav_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XYNav_Example : NSObject
-@end
-@implementation PodsDummy_Pods_XYNav_Example
-@end
--- XYNav/Example/Pods/Target Support Files/Pods-XYNav_Tests/Pods-XYNav_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XYNav_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_XYNav_Tests
-@end
